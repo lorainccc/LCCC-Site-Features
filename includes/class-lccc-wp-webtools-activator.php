016174978cfa798e749dc970c7ba1c9a4757a0e1
Joe Querin <joecue@gmail.com>
2016-09-09 11:39:49 -0400

    Initial Commit

---
+++ includes/class-lccc-wp-webtools-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.lorainccc.edu
+ * @since      1.0.0
+ *
+ * @package    Lccc_Wp_Webtools
+ * @subpackage Lccc_Wp_Webtools/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Lccc_Wp_Webtools
+ * @subpackage Lccc_Wp_Webtools/includes
+ * @author     LCCC Web Dev Team <notice@lorainccc.edu>
+ */
+class Lccc_Wp_Webtools_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
