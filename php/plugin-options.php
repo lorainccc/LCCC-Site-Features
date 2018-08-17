a3dd9042b9b507230bc7aeb97dda79c6c8dd765d
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-08-17 16:10:17 -0400

    :wrench: Adding Site Featured Image Support

--- php/plugin-options.php
+++ php/plugin-options.php
@@ -32,6 +32,14 @@ function lc_add_wp_webtools_menu_page() {
   'lc-media-files',                                                   			// Menu Slug
   'lc_media_files_list'                                                  // Function
  );
+		add_submenu_page(
+		'lccc-wp-webtools',																																																		  // Parent Slug (Page to nest under)
+  __( 'Site Featured Image', 'lorainccc' ),   // Page Title
+  'Site Featured Image',                                                 // Menu Title
+  'manage_options',                                                      // Capabilities
+  'lc-site-featured-image',                                              // Menu Slug
+  'lc_site_featured_featured_field'                                      // Function
+ );
 }
 
 add_action( 'admin_menu', 'lc_add_wp_webtools_menu_page' );
@@ -400,4 +408,99 @@ if( ! ( function_exists( 'lc_get_attachment_by_name' ) ) ) {
     }
 }
 
-?>
+//Site Featured Image Field
+
+function lc_site_featured_featured_field(){
+	?>
+
+<h1>Site Featured Image</h1>
+<p>If the template supports the custom field, a featured image will appear at the top of the page below the navigation bar.</p>
+<p>Use the upload image button at the bottom to select an image from the Media Gallery or upload a new image to the Gallery.  After selecting, click the save button to save your selection.</p>
+
+<?php
+	
+	// Save attachment ID
+	if ( isset( $_POST['submit_image_selector'] ) && isset( $_POST['image_attachment_id'] ) ) :
+		update_option( 'lc_site_featured_image_id', absint( $_POST['image_attachment_id'] ) );
+	endif;
+
+	wp_enqueue_media();
+
+	?><form method='post'>
+		<div class='image-preview-wrapper'>
+			<img id='image-preview' src='<?php echo wp_get_attachment_url( get_option( 'lc_site_featured_image_id' ) ); ?>' height='100'>
+		</div>
+		<input id="upload_image_button" type="button" class="button" value="<?php _e( 'Upload image' ); ?>" />
+		<input type='hidden' name='image_attachment_id' id='image_attachment_id' value='<?php echo get_option( 'lc_site_featured_image_id' ); ?>'>
+		<input type="submit" name="submit_image_selector" value="Save" class="button-primary">
+	</form><?php
+
+
+}	
+
+add_action( 'admin_footer', 'media_selector_print_scripts' );
+
+function media_selector_print_scripts() {
+
+	$my_saved_attachment_post_id = get_option( 'lc_site_featured_image_id', 0 );
+
+	?><script type='text/javascript'>
+
+		jQuery( document ).ready( function( $ ) {
+
+			// Uploading files
+			var file_frame;
+			var wp_media_post_id = wp.media.model.settings.post.id; // Store the old id
+			var set_to_post_id = <?php echo $my_saved_attachment_post_id; ?>; // Set this
+
+			jQuery('#upload_image_button').on('click', function( event ){
+
+				event.preventDefault();
+
+				// If the media frame already exists, reopen it.
+				if ( file_frame ) {
+					// Set the post ID to what we want
+					file_frame.uploader.uploader.param( 'post_id', set_to_post_id );
+					// Open frame
+					file_frame.open();
+					return;
+				} else {
+					// Set the wp.media post id so the uploader grabs the ID we want when initialised
+					wp.media.model.settings.post.id = set_to_post_id;
+				}
+
+				// Create the media frame.
+				file_frame = wp.media.frames.file_frame = wp.media({
+					title: 'Select a image to upload',
+					button: {
+						text: 'Use this image',
+					},
+					multiple: false	// Set to true to allow multiple files to be selected
+				});
+
+				// When an image is selected, run a callback.
+				file_frame.on( 'select', function() {
+					// We set multiple to false so only get one image from the uploader
+					attachment = file_frame.state().get('selection').first().toJSON();
+
+					// Do something with attachment.id and/or attachment.url here
+					$( '#image-preview' ).attr( 'src', attachment.url ).css( 'width', 'auto' );
+					$( '#image_attachment_id' ).val( attachment.id );
+
+					// Restore the main post ID
+					wp.media.model.settings.post.id = wp_media_post_id;
+				});
+
+					// Finally, open the modal
+					file_frame.open();
+			});
+
+			// Restore the main ID when the add media button is pressed
+			jQuery( 'a.add_media' ).on( 'click', function() {
+				wp.media.model.settings.post.id = wp_media_post_id;
+			});
+		});
+
+	</script><?php
+
+} ?>
