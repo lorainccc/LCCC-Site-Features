c58bb7a333a7d61f2470ab8d1a74ab5826a8121e
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-09-06 14:42:28 -0400

    :wrench: Added Logic to Check for Name for Notification

--- php/default-features/content-approvals/notifications.php
+++ php/default-features/content-approvals/notifications.php
@@ -26,7 +26,13 @@
 				$edit_page_url = get_bloginfo( 'url' );
 				$edit_page_url = $edit_page_url . '/wp-admin/post.php?action=edit&post=' . $post->ID;
 
-				$body = '<img src="http://www.lorainccc.edu/wp-content/themes/lorainccc/images/LCCC-Logo.png" style="width:285px; height:59px;"><br/><h1 style="font-size: 16pt;font-family:sans-serif;">Page Approval Notice</h1><p style="font-size: 12pt;font-family:sans-serif;">A pending revision to the Page "' . $post->post_title . '" </p><p style="font-size: 12pt;font-family:sans-serif;">It was submitted by ' . $current_user->user_firstname . ' ' . $current_user->user_lastname . '.</p><p style="font-size: 12pt;font-family:sans-serif;">Review it here:<a href="' . $edit_page_url . '" target="_blank">' . $edit_page_url . '</a></p>';
+				if( $current_user->user_firstname != '' ){
+					$submitted_by = $current_user->user_firstname . ' ' . $current_user->user_lastname;
+				}else{
+					$submitted_by = $current_user->user_login;
+				}
+										
+				$body = '<img src="http://www.lorainccc.edu/wp-content/themes/lorainccc/images/LCCC-Logo.png" style="width:285px; height:59px;"><br/><h1 style="font-size: 16pt;font-family:sans-serif;">Page Approval Notice</h1><p style="font-size: 12pt;font-family:sans-serif;">A pending revision to the Page "' . $post->post_title . '" </p><p style="font-size: 12pt;font-family:sans-serif;">It was submitted by ' . $submitted_by . '.</p><p style="font-size: 12pt;font-family:sans-serif;">Review it here:<a href="' . $edit_page_url . '" target="_blank">' . $edit_page_url . '</a></p>';
 
 				$headers = array('Content-Type: text/html; charset=UTF-8');
 
