0cee62eb5faabb6cbf50f248f9bb14c560374781
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-09-07 16:16:08 -0400

    :wrench: Return user to posts screen | Updated Search Widget IDs

--- php/default-features/lc-search-widget.php
+++ php/default-features/lc-search-widget.php
@@ -6,7 +6,7 @@ function lc_search_form( $form ) {
 	$form .= '        <span class="screen-reader-text" for="s">' . __( 'Search for:' ) .'</span>';
  $form .= '   <input type="text" value="' . get_search_query() . '" name="s" id="s" />';
 	$form .= '   </label>';
-	$form .= '			<input type="hidden" name="searchblogs" value="1,2,3,4,5,7,8,9,10,12,13,14,16,17,18,19,20,21,22,23,24,25,26,28,29,30,31,32,33,34,35,37,38,39,40,41,42,44,45,46,47,48,49,50,51,52,54,55,56,57,57,58,59,60,60,61,62,63,64,65,67,68,69,70,71,72,73,74,76" />';
+	$form .= '			<input type="hidden" name="searchblogs" value="1,2,3,4,5,7,8,9,10,12,13,14,16,17,18,19,20,21,22,23,24,25,26,28,29,30,31,32,33,34,35,37,38,39,40,41,42,44,45,46,47,48,49,50,51,52,54,55,56,57,57,58,59,60,60,61,62,63,64,65,67,68,69,70,71,72,73,74,76,77" />';
 	$form .= '    <input type="submit" class="search-submit" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'" />';
 	$form .= '				</div>';
 	$form .= '</form>';
