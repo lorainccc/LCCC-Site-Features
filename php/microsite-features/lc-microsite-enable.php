f0b4af4aca536da6bca716cd6d4131a25ad24d14
Joe <joecue@gmail.com>
2021-04-22 16:18:12 -0400

    :wrench: Removing Gateway Menu
    Transitioned all Gateway Menu Content to Content Group CPT

--- php/microsite-features/lc-microsite-enable.php
+++ php/microsite-features/lc-microsite-enable.php
@@ -7,8 +7,9 @@
 
 // Metabox
 
-require_once( plugin_dir_path( __FILE__ ).'../gateway/widget-gateway-cpt.php');
-require_once( plugin_dir_path( __FILE__ ).'../gateway/widget-gateway-menu.php');
+
+//require_once( plugin_dir_path( __FILE__ ).'../gateway/widget-gateway-cpt.php');
+//require_once( plugin_dir_path( __FILE__ ).'../gateway/widget-gateway-menu.php');
 
 require_once( plugin_dir_path( __FILE__ ).'lc-microsite-metabox.php' );
 require_once( plugin_dir_path( __FILE__ ).'lc-content_group-ctp.php' );
